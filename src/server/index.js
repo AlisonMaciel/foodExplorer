9d1e7fb0b171bf3225507413a019c606d009f4ee
Alison <alisonbetini27@gmail.com>
2025-03-20 16:49:33 -0300

    alteração de rota para https://api-food-vzfi.onrender.com

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://api-food-vzfi.onrender.com/dish",
+  baseURL: "https://api-food-vzfi.onrender.com",
   withCredentials: true
 })
